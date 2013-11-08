62a48d5ab63934fe58217429e5b8d7b8630f69b4
nadir93 <nadir93@gmail.com>
2013-11-08 13:46:03 +0900

    debug -> info

--- mqtt/src/main/java/kr/co/adflow/netty/HubNSpokeServer.java
+++ mqtt/src/main/java/kr/co/adflow/netty/HubNSpokeServer.java
@@ -34,13 +34,13 @@ public class HubNSpokeServer {
 				boot.group(bossGroup, workerGroup)
 						.channel(NioServerSocketChannel.class)
 						.option(ChannelOption.SO_BACKLOG, 100)
-						.handler(new LoggingHandler(LogLevel.DEBUG))
+						.handler(new LoggingHandler(LogLevel.INFO))
 						.childHandler(new ChannelInitializer<SocketChannel>() {
 							@Override
 							public void initChannel(SocketChannel ch)
 									throws Exception {
 								ch.pipeline().addLast(
-								// new LoggingHandler(LogLevel.INFO),
+								 new LoggingHandler(LogLevel.INFO),
 										new BroadCastHandler());
 							}
 						});
