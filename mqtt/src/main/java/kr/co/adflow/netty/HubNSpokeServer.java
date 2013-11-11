bcbbff8b72ebc54eae3481809c2fb8b40f233320
nadir93 <nadir93@gmail.com>
2013-11-11 13:18:48 +0900

    logLevel error

--- mqtt/src/main/java/kr/co/adflow/netty/HubNSpokeServer.java
+++ mqtt/src/main/java/kr/co/adflow/netty/HubNSpokeServer.java
@@ -51,13 +51,13 @@ public class HubNSpokeServer {
 				boot.group(bossGroup, workerGroup)
 						.channel(NioServerSocketChannel.class)
 						.option(ChannelOption.SO_BACKLOG, 100)
-						.handler(new LoggingHandler(LogLevel.INFO))
+						.handler(new LoggingHandler(LogLevel.ERROR))
 						.childHandler(new ChannelInitializer<SocketChannel>() {
 							@Override
 							public void initChannel(SocketChannel ch)
 									throws Exception {
 								ch.pipeline().addLast(
-										new LoggingHandler(LogLevel.INFO),
+										new LoggingHandler(LogLevel.ERROR),
 										new BroadCastHandler());
 							}
 						});
