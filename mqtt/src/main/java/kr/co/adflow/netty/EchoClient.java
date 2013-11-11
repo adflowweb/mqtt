30f414f93926339ec13a4cef00c57075d92b3332
nadir93 <nadir93@gmail.com>
2013-11-11 10:50:59 +0900

    test

--- mqtt/src/main/java/kr/co/adflow/netty/EchoClient.java
+++ mqtt/src/main/java/kr/co/adflow/netty/EchoClient.java
@@ -26,73 +26,70 @@ import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
 
 /**
- * Sends one message when a connection is open and echoes back any received
- * data to the server.  Simply put, the echo client initiates the ping-pong
- * traffic between the echo client and server by sending the first message to
- * the server.
+ * Sends one message when a connection is open and echoes back any received data
+ * to the server. Simply put, the echo client initiates the ping-pong traffic
+ * between the echo client and server by sending the first message to the
+ * server.
  */
 public class EchoClient {
 
-    private final String host;
-    private final int port;
-    private final int firstMessageSize;
+	private final String host;
+	private final int port;
+	private final int firstMessageSize;
 
-    public EchoClient(String host, int port, int firstMessageSize) {
-        this.host = host;
-        this.port = port;
-        this.firstMessageSize = firstMessageSize;
-    }
+	public EchoClient(String host, int port, int firstMessageSize) {
+		this.host = host;
+		this.port = port;
+		this.firstMessageSize = firstMessageSize;
+	}
 
-    public void run() throws Exception {
-        // Configure the client.
-        EventLoopGroup group = new NioEventLoopGroup();
-        try {
-            Bootstrap b = new Bootstrap();
-            b.group(group)
-             .channel(NioSocketChannel.class)
-             .option(ChannelOption.TCP_NODELAY, true)
-             .handler(new ChannelInitializer<SocketChannel>() {
-                 @Override
-                 public void initChannel(SocketChannel ch) throws Exception {
-                     ch.pipeline().addLast(
-                             //new LoggingHandler(LogLevel.INFO),
-                             new EchoClientHandler(firstMessageSize));
-                 }
-             });
+	public void run() throws Exception {
+		// Configure the client.
+		EventLoopGroup group = new NioEventLoopGroup();
+		try {
+			Bootstrap b = new Bootstrap();
+			b.group(group).channel(NioSocketChannel.class)
+					.option(ChannelOption.TCP_NODELAY, true)
+					.handler(new ChannelInitializer<SocketChannel>() {
+						@Override
+						public void initChannel(SocketChannel ch)
+								throws Exception {
+							ch.pipeline().addLast(
+							// new LoggingHandler(LogLevel.INFO),
+									new EchoClientHandler(firstMessageSize));
+						}
+					});
 
-            
-            
-            
-            // Start the client.
-            ChannelFuture f = b.connect(host, port).sync();
+			// Start the client.
+			ChannelFuture f = b.connect(host, port).sync();
 
-            // Wait until the connection is closed.
-            f.channel().closeFuture().sync();
-        } finally {
-            // Shut down the event loop to terminate all threads.
-            group.shutdownGracefully();
-        }
-    }
+			// Wait until the connection is closed.
+			f.channel().closeFuture().sync();
+		} finally {
+			// Shut down the event loop to terminate all threads.
+			group.shutdownGracefully();
+		}
+	}
 
-    public static void main(String[] args) throws Exception {
-        // Print usage if no argument is specified.
-        if (args.length < 2 || args.length > 3) {
-            System.err.println(
-                    "Usage: " + EchoClient.class.getSimpleName() +
-                    " <host> <port> [<first message size>]");
-            return;
-        }
+	public static void main(String[] args) throws Exception {
+		System.out.println("EchoClient starting...");
+		// Print usage if no argument is specified.
+		if (args.length < 2 || args.length > 3) {
+			System.err.println("Usage: " + EchoClient.class.getSimpleName()
+					+ " <host> <port> [<first message size>]");
+			return;
+		}
 
-        // Parse options.
-        final String host = args[0];
-        final int port = Integer.parseInt(args[1]);
-        final int firstMessageSize;
-        if (args.length == 3) {
-            firstMessageSize = Integer.parseInt(args[2]);
-        } else {
-            firstMessageSize = 256;
-        }
+		// Parse options.
+		final String host = args[0];
+		final int port = Integer.parseInt(args[1]);
+		final int firstMessageSize;
+		if (args.length == 3) {
+			firstMessageSize = Integer.parseInt(args[2]);
+		} else {
+			firstMessageSize = 256;
+		}
 
-        new EchoClient(host, port, firstMessageSize).run();
-    }
+		new EchoClient(host, port, firstMessageSize).run();
+	}
 }
