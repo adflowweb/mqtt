1197dec9eba0e75ff7b5454749163ebe23385a5c
nadir93 <nadir93@gmail.com>
2013-11-11 10:40:06 +0900

    echo client referenceCountUtil release

--- mqtt/src/main/java/kr/co/adflow/netty/handler/EchoClientHandler.java
+++ mqtt/src/main/java/kr/co/adflow/netty/handler/EchoClientHandler.java
@@ -19,54 +19,61 @@ import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.util.ReferenceCountUtil;
 
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
- * Handler implementation for the echo client.  It initiates the ping-pong
+ * Handler implementation for the echo client. It initiates the ping-pong
  * traffic between the echo client and server by sending the first message to
  * the server.
  */
 public class EchoClientHandler extends ChannelInboundHandlerAdapter {
 
-    private static final Logger logger = Logger.getLogger(
-            EchoClientHandler.class.getName());
+	private static final Logger logger = Logger
+			.getLogger(EchoClientHandler.class.getName());
 
-    private final ByteBuf firstMessage;
+	private final ByteBuf firstMessage;
 
-    /**
-     * Creates a client-side handler.
-     */
-    public EchoClientHandler(int firstMessageSize) {
-        if (firstMessageSize <= 0) {
-            throw new IllegalArgumentException("firstMessageSize: " + firstMessageSize);
-        }
-        firstMessage = Unpooled.buffer(firstMessageSize);
-        for (int i = 0; i < firstMessage.capacity(); i ++) {
-            firstMessage.writeByte((byte) i);
-        }
-    }
+	/**
+	 * Creates a client-side handler.
+	 */
+	public EchoClientHandler(int firstMessageSize) {
+		if (firstMessageSize <= 0) {
+			throw new IllegalArgumentException("firstMessageSize: "
+					+ firstMessageSize);
+		}
+		firstMessage = Unpooled.buffer(firstMessageSize);
+		for (int i = 0; i < firstMessage.capacity(); i++) {
+			firstMessage.writeByte((byte) i);
+		}
+	}
 
-    @Override
-    public void channelActive(ChannelHandlerContext ctx) {
-        ctx.writeAndFlush(firstMessage);
-    }
+	@Override
+	public void channelActive(ChannelHandlerContext ctx) {
+		ctx.writeAndFlush(firstMessage);
+	}
 
-    @Override
-    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
-        ctx.write(msg);
-    }
+	@Override
+	public void channelRead(ChannelHandlerContext ctx, Object msg)
+			throws Exception {
+		try {
+			ctx.writeAndFlush(msg);
+		} finally {
+			ReferenceCountUtil.release(msg);
+		}
+	}
 
-    @Override
-    public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
-       ctx.flush();
-    }
+	@Override
+	public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+	}
 
-    @Override
-    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
-        // Close the connection when an exception is raised.
-        logger.log(Level.WARNING, "Unexpected exception from downstream.", cause);
-        ctx.close();
-    }
+	@Override
+	public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+		// Close the connection when an exception is raised.
+		logger.log(Level.WARNING, "Unexpected exception from downstream.",
+				cause);
+		ctx.close();
+	}
 }
