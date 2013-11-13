2079a5f1385b0d415f5a7c3e787a9a33fec64bf4
nadir93 <nadir93@gmail.com>
2013-11-13 11:24:00 +0900

    echoclient port 변경 4883 -> 3883

--- mqtt/echoClient.sh
+++ mqtt/echoClient.sh
@@ -1,1 +1,1 @@
-java -Xms512m -Xmx512m -XX:NewRatio=3 -XX:+PrintGCTimeStamps -XX:+PrintGCDetails -Xloggc:echo_client_gc.log -cp ./target/classes:$HOME/.m2/repository/io/netty/netty-all/4.0.11.Final/netty-all-4.0.11.Final.jar kr.co.adflow.netty.EchoClient 127.0.0.1 4883
+java -Xms512m -Xmx512m -XX:NewRatio=3 -XX:+PrintGCTimeStamps -XX:+PrintGCDetails -Xloggc:echo_client_gc.log -cp ./target/classes:$HOME/.m2/repository/io/netty/netty-all/4.0.11.Final/netty-all-4.0.11.Final.jar kr.co.adflow.netty.EchoClient 127.0.0.1 3883
