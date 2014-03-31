48f83b951b91ec80488243f6e6f6a29f71336f95
nadir93 <nadir93@gmail.com>
2014-03-31 10:54:04 +0900

    별자리운세서비스 유저추가

--- pushFortune/pushZodiac.js
+++ pushFortune/pushZodiac.js
@@ -16,9 +16,10 @@ var mqtt = require('mqtt')
 var serverIP = '175.209.8.188';
 var port = 1883;
 //추후 개인데이타는 암호화 하던가 또는 파일이나 디비에서 가져오도록..
-var users = {"df33406434de552faf60efa": {"name": "이은영", "birth": {"year": 1978, "month": 3, "day": 8}},
+var users = {"ba54e5ed3b8aace5e1ce833": {"name": "이찬호", "birth": {"year": 1982, "month": 10, "day": 20}},
+  "df33406434de552faf60efa": {"name": "이은영", "birth": {"year": 1978, "month": 3, "day": 8}},
   "1c45de7cc1daa896bfd32dc": {"name": "박택영", "birth": {"year": 1974, "month": 12, "day": 27}}};
-var start = new Date(2014, 2, 30, 07, 00, 00);
+var start = new Date(2014, 4, 1, 07, 00, 00);
 var images = new Array();
 
 //푸시에사용할 별자리이미지로딩...
@@ -182,7 +183,7 @@ function remaining(target) {
   var now = new Date();
   var nt = now.getTime();
   var et = target.getTime();
-  return parseInt(et - nt) 
+  return parseInt(et - nt)
 }
 
 
