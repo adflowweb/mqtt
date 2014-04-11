ba677f6ddc0ff1490067a30271e077f1d315439e
nadir93 <nadir93@gmail.com>
2014-04-11 13:07:57 +0900

    프로젝트 이름 변경 pushServer -> pushServer

--- pushService/src/main/java/kr/co/adflow/push/dao/AuthDAO.java
+++
@@ -1,5 +0,0 @@
-package kr.co.adflow.push.dao;
-
-public interface AuthDAO {
-
-}
--- pushService/src/main/java/kr/co/adflow/push/dao/impl/AuthDAOImpl.java
+++
@@ -1,7 +0,0 @@
-package kr.co.adflow.push.dao.impl;
-
-import kr.co.adflow.push.dao.AuthDAO;
-
-public class AuthDAOImpl implements AuthDAO {
-
-}
