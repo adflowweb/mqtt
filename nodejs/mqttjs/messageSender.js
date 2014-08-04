bd92c344ea2de5450528b7b5b868a67d27a6c85b
nadir93 <nadir93@gmail.com>
2014-08-04 14:55:59 +0900

    messageSender.js 성능테스트

--- nodejs/mqttjs/messageSender.js
+++ nodejs/mqttjs/messageSender.js
@@ -5,34 +5,60 @@
  */
 var mqtt = require('mqtt');
 var index = 0;
-var loopCount = 100;
+var loopCount = 10000;
 //var client = mqtt.createClient(1883, '192.168.0.21');
-var client = mqtt.createClient(1883, '172.30.1.60');
+var client = mqtt.createClient(1883, '14.63.216.249');
+var callbackCnt = 1;
+//var client = mqtt.createClient(3881, 'adflow.net');
 //var client = mqtt.createClient(1883, 'test.mosquitto.org');
 
-client.subscribe('testTopicAdflow');
-client.on('message', function (topic, message) {
-    console.log(message);
-});
+//client.subscribe('/push/poll');
+//client.on('message', function (topic, message) {
+//  console.log(message);
+//});
 
 //setInterval(publish, 1000);
 
-console.log('loopCount::'+loopCount);
+console.log('loopCount=' + loopCount);
 
-setTimeout(publish,500);
+setTimeout(publish, 1);
 
-function publish(){
+var start = new Date().getTime();
 
-    if(index < loopCount)
-    {
-        console.log('index::'+(index+1));
-        client.publish('testTopicAdflow', (index+1)+' 0123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789', {'qos':2});
-        index++;
-        setTimeout(publish,500);
-    } else {
-      console.log('theEnd');
-      //process.exit(0);
-    }
+function publish() {
+
+
+  for (var i = 0; i < loopCount; i++) {
+    //console.log('index::' + (index + 1));
+    client.publish('/push/test001', (index + 1) + ' 0123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789', {'qos': 2}, function (err) {
+      console.log('callbacked' + (callbackCnt++));
+      if (callbackCnt == 10000) {
+        var end = new Date().getTime();
+        var time = end - start;
+        console.log('걸린시간=' + time);
+      }
+      if (err) throw err;
+    });
+    index++;
+  }
+
+
+
+
+  //  if (index < loopCount) {
+  //    console.log('index::' + (index + 1));
+  //    client.publish('/push/test', (index+1)+' 0123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789', {'qos':2});
+  //    //client.publish('/push/poll', '{"id":1,"userid":"nadir93","answerid":'+(index+1)+'}', {'qos': 2});
+  //
+  //    index++;
+  //    setTimeout(publish, 1);
+  //  } else {
+  //    var end = new Date().getTime();
+  //    var time = end - start;
+  //    console.log('걸린시간='+time);
+  //    console.log('theEnd');
+  //    //process.exit(0);
+  //  }
 
 }
 
