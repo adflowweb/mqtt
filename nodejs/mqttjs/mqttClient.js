a32f9aeeba35f909499b77148a8cd6d58a20534c
nadir93 <nadir93@gmail.com>
2014-08-06 13:44:00 +0900

    ktp 성능테스트용 수정

--- nodejs/mqttjs/mqttClient.js
+++ nodejs/mqttjs/mqttClient.js
@@ -3,63 +3,76 @@
  * Date: 2014. 2. 7.
  * Time: 오후 1:32
  */
-var mqtt = require('mqtt');
+var mqtt = require('../../../MQTT.js');
 var util = require('util');
 var index = 1;
 var clients = new Array();
-var clientCount = 1;
+var clientCount = 100;
+var connectedCnt = 1;
+var cluster = require('cluster');
+var numCPUs = require('os').cpus().length;
+pingCnt = 1;
 
-for (i = 0; i < clientCount; i++) {
+if (cluster.isMaster) {
+  // Fork workers.
+  for (var i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
 
-  //clients[i] = mqtt.createClient(1883, 'adflow.net');
-
-  //clients[i] = mqtt.createClient(1883, '192.168.0.21', {clientId: 'example' + i, clean: false});
-  clients[i] = mqtt.createClient(1883, '14.63.216.249', {clientId: 'example' + i, clean: false});
-
-  //  clients[i] = mqtt.createClient(1883, '192.168.0.21', function (err, client) {
-  //    if (err) throw err;
-  //
-  //    client.connect({
-  //      protocolId: 'MQIsdp',
-  //      protocolVersion: 3,
-  //      clientId: 'example' + i,
-  //      keepalive: 30000,
-  //      clean: false
-  //    });
-  //  });
-  //clients[i] = mqtt.createClient(1883, '172.30.1.60');
-  //clients[i] = mqtt.createClient(1883, 'test.mosquitto.org');
-  //if (i == 1) {
-  //  clients[i].subscribe('/push/test1');
-  //} else {
-  clients[i].subscribe('/push/test001', {qos: 2}, function (err, granted) {
-    if (err) throw err;
-    console.log('granted=' + util.inspect(granted));
+  cluster.on('exit', function (worker, code, signal) {
+    console.log('worker ' + worker.process.pid + ' died');
   });
 
-
-  // }
-
-  //clients[i].subscribe('/push/test');
-  clients[i].on('message', function (topic, message) {
-
-    if (index == 1) {
-      start = new Date().getTime();
-      console.log('시작시간=' + start);
-    }
-
-    //var j = message.substring(0,1);
-    //index = index + parseInt(j);
-    console.log('index=' + (index++));
-    //console.log(message);
-
-    if (index == 10000) {
-      end = new Date().getTime();
-      time = end - start;
-      console.log('걸린시간=' + time + " ms");
-    }
+  cluster.on('fork', function (worker) {
+    console.log('worker=' + worker.process.pid);
   });
+
+} else {
+  for (i = 0; i < clientCount; i++) {
+
+    //clients[i] = mqtt.createClient(1883, 'adflow.net');
+    //clients[i] = mqtt.createClient(1883, '192.168.0.21', {clientId: 'example' + i, clean: false});
+    //clients[i] = mqtt.createClient(1883, '14.63.216.249', {clientId: 'examples' + i, clean: false});
+    clients[i] = mqtt.createClient(1883, '14.63.216.249', {keepalive: 5});
+
+    //clients[i] = mqtt.createClient(1883, '172.30.1.60');
+    //clients[i] = mqtt.createClient(1883, 'test.mosquitto.org');
+    //if (i == 1) {
+    //  clients[i].subscribe('/push/test1');
+    //} else {
+    clients[i].subscribe('/push/test002', {qos: 2}, function (err, granted) {
+      if (err) throw err;
+      console.log('connected=' + (connectedCnt++) + util.inspect(granted));
+    });
+
+
+    // }
+
+    //clients[i].subscribe('/push/test');
+    clients[i].on('message', function (topic, message) {
+
+      if (index == 1) {
+        start = new Date().getTime();
+        console.log('시작시간=' + start);
+      }
+
+      //var j = message.substring(0,1);
+      //index = index + parseInt(j);
+      console.log('index=' + (index++));
+      //console.log(message);
+
+      if ((index % 100) == 0) {
+        end = new Date().getTime();
+        time = end - start;
+        console.log('걸린시간=' + time + " ms");
+      }
+    });
+
+  }
+
+  console.log('clientsInited=' + clients.length);
 
 }
 
-console.log('clientsInited=' + clients.length);
+
+
