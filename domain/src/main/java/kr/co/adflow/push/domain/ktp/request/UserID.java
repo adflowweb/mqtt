7202b69fddf70a9e47d2b1f2b57f009b4fc955ef
joshua2415 <joshua90@gmail.com>
2015-01-15 02:31:08 +0900

    1. 추가 api

---
+++ domain/src/main/java/kr/co/adflow/push/domain/ktp/request/UserID.java
@@ -0,0 +1,32 @@
+/*
+ * 
+ */
+package kr.co.adflow.push.domain.ktp.request;
+
+// TODO: Auto-generated Javadoc
+/**
+ * The Class UserID.
+ */
+public class UserID {
+	
+	/** The user id. */
+	private String userID;
+
+	/**
+	 * Gets the user id.
+	 *
+	 * @return the user id
+	 */
+	public String getUserID() {
+		return userID;
+	}
+
+	/**
+	 * Sets the user id.
+	 *
+	 * @param userID the new user id
+	 */
+	public void setUserID(String userID) {
+		this.userID = userID;
+	}
+}
---
+++ domain/src/main/java/kr/co/adflow/push/domain/ktp/request/UserMessage.java
@@ -0,0 +1,141 @@
+/*
+ * 
+ */
+package kr.co.adflow.push.domain.ktp.request;
+
+
+// TODO: Auto-generated Javadoc
+/**
+ * The Class UserMessage.
+ */
+public class UserMessage {
+
+	/** The content. */
+	private String content;
+	
+	/** The sender. */
+	private String sender;
+	
+	/** The receiver. */
+	private String receiver;
+	
+	/** The content type. */
+	private String contentType;
+	
+	/** The qos. */
+	private int qos;
+	
+	/** The expiry. */
+	private long expiry;
+	//private int type;
+	
+	/**
+	 * Gets the content.
+	 *
+	 * @return the content
+	 */
+	public String getContent() {
+		return content;
+	}
+	
+	/**
+	 * Sets the content.
+	 *
+	 * @param content the new content
+	 */
+	public void setContent(String content) {
+		this.content = content;
+	}
+	
+	/**
+	 * Gets the sender.
+	 *
+	 * @return the sender
+	 */
+	public String getSender() {
+		return sender;
+	}
+	
+	/**
+	 * Sets the sender.
+	 *
+	 * @param sender the new sender
+	 */
+	public void setSender(String sender) {
+		this.sender = sender;
+	}
+	
+	/**
+	 * Gets the receiver.
+	 *
+	 * @return the receiver
+	 */
+	public String getReceiver() {
+		return receiver;
+	}
+	
+	/**
+	 * Sets the receiver.
+	 *
+	 * @param receiver the new receiver
+	 */
+	public void setReceiver(String receiver) {
+		this.receiver = receiver;
+	}
+	
+	/**
+	 * Gets the content type.
+	 *
+	 * @return the content type
+	 */
+	public String getContentType() {
+		return contentType;
+	}
+	
+	/**
+	 * Sets the content type.
+	 *
+	 * @param contentType the new content type
+	 */
+	public void setContentType(String contentType) {
+		this.contentType = contentType;
+	}
+	
+	/**
+	 * Gets the qos.
+	 *
+	 * @return the qos
+	 */
+	public int getQos() {
+		return qos;
+	}
+	
+	/**
+	 * Sets the qos.
+	 *
+	 * @param qos the new qos
+	 */
+	public void setQos(int qos) {
+		this.qos = qos;
+	}
+	
+	/**
+	 * Gets the expiry.
+	 *
+	 * @return the expiry
+	 */
+	public long getExpiry() {
+		return expiry;
+	}
+	
+	/**
+	 * Sets the expiry.
+	 *
+	 * @param expiry the new expiry
+	 */
+	public void setExpiry(long expiry) {
+		this.expiry = expiry;
+	}
+
+
+}
